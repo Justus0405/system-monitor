689786fd11d7ab72f9489a0c9818c087e7121f81
Justus0405 <justus040506@gmail.com>
2024-12-20 22:34:10 +0100

    Update to Version 0.3

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-# system-monitor
+# System-monitor
 A System monitoring script for Linux written in bash
 
-# Warning
-This is work in progress
-
-# dependencies
+# Dependencies
 ```shell
 bash
 ```
 
-# installation & usage
+# Installation
 ```shell
 git clone https://github.com/Justus0405/system-monitor.git
 cd system-monitor
 chmod +x system-monitor.sh
 ./system-monitor.sh
+```
+# Usage
+```plaintext
+    usage:  system-monitor [...]
+    arguments:
+        -h | --help
+        -t | --temps
 ```
--- system-monitor.sh
+++ system-monitor.sh
@@ -1,46 +1,23 @@
 #!/bin/bash
-clear
 
-VERSION=0.2
-
-## Dashboard Functions
+# Color Variables
+BLACK="\e[30m"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+WHITE="\e[97m"
+ENDCOLOR="\e[0m"
 
 TOP_BAR() {
-	echo -e "┌───┤< \e[1;32mSystem Monitor\e[0m >"
-	echo -e "│"
-}
-
-USAGE() {
-	# Try calculate current CPU usage
-	cpu_usage=$(grep 'cpu ' /proc/stat | awk '{usage=100-($5*100)/($2+$3+$4+$5+$6+$7+$8)} END {printf "%.0f", usage}')
-
-	# Show ram usage in mb, cut everything except line 2, calculate used percentage
-	ram_usage=$(free -m | awk 'NR==2{used=$3; total=$2; avail=$7; printf "%dmb / %dmb (%.0f%%), available: %dmb\n", used, total, used/total*100, avail}')
-
-	# Show total disk usage in gb, only use line total , print values
-	disk_usage=$(df -h --total | awk '/total/ {printf "%s / %s (%s), available: %s\n", $3, $2, $5, $4}')
-
-	echo -e "│	┌─[\e[1;35mSystem Usage\e[0m]"
-	echo -e "│	├ <\e[1;34mCpu\e[0m>      $cpu_usage%"
-	echo -e "│	├ <\e[1;34mRam\e[0m>      $ram_usage"
-	echo -e "│	├ <\e[1;34mDisk\e[0m>     $disk_usage"
-	echo -e "│"
-}
-
-NETWORK() {
-	# Tries to ping google dns server, if successful return Online, if not return Offline
-	online_status=$(if ping -c 1 8.8.8.8 &> /dev/null; then echo -e "\e[1;32mOnline\e[0m"; else echo -e "\e[1;31mOffline\e[0m"; fi)
-
-	# Show every IPv4 adress, filter only for the numbers, remove loopback adress, show remaining adress
-	local_ip=$(ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v '127.0.0.1' | head -n 1)
-
-	echo -e "│	┌─[\e[1;35mNetwork\e[0m]"
-	echo -e "│	├ <\e[1;34mInternet\e[0m> $online_status"
-	echo -e "│	├ <\e[1;34mLocal IP\e[0m> $local_ip"
-	echo -e "│"
+	echo -e ""
+	echo -e "\t╭─────────────╮"
 }
 
 OS() {
+	USER=$(whoami)
 	KERNEL=$(uname -r)
 	UPTIME=$(uptime -p)
 	# Reads the age of the file machine-id to determine the OS age
@@ -50,28 +27,62 @@ OS() {
 		AGE="Error: File not found"
 	fi
 
-	echo -e "│	┌─[\e[1;35mOS\e[0m]"
-	echo -e "│	├ <\e[1;34mKernel\e[0m>   $KERNEL"
-	echo -e "│	├ <\e[1;34mUptime\e[0m>   $UPTIME"
-	echo -e "│	├ <\e[1;34mAge\e[0m>      $AGE days"
-	echo -e "│"
+	echo -e "\t│   ${RED}User${ENDCOLOR}      │ $USER"
+	echo -e "\t│   ${YELLOW}Kernel${ENDCOLOR}    │ $KERNEL"
+	echo -e "\t│   ${BLUE}Uptime${ENDCOLOR}    │ $UPTIME"
+	echo -e "\t│   ${GREEN}Age${ENDCOLOR}       │ $AGE days"
+	echo -e "\t├─────────────┤"
+}
+
+USAGE() {
+	# Try calculate current CPU usage does not work
+	# cpu_usage=$(grep 'cpu ' /proc/stat | awk '{usage=100-($5*100)/($2+$3+$4+$5+$6+$7+$8)} END {printf "%.0f", usage}')
+
+	# Show ram usage in mb, cut everything except line 2, calculate used percentage
+	ram_usage=$(free -m | awk 'NR==2{used=$3; total=$2; avail=$7; printf "%dmb / %dmb (%.0f%%), available: %dmb\n", used, total, used/total*100, avail}')
+
+	# Show total disk usage in gb, only use line total , print values
+	disk_usage=$(df -h --total | awk '/total/ {printf "%s / %s (%s), available: %s\n", $3, $2, $5, $4}')
+
+	# echo -e "\t│   ${RED}Cpu${ENDCOLOR}       │ $cpu_usage%"
+	echo -e "\t│   ${MAGENTA}Ram${ENDCOLOR}       │ $ram_usage"
+	echo -e "\t│   ${CYAN}Disk${ENDCOLOR}      │ $disk_usage"
+	echo -e "\t├─────────────┤"
+}
+
+NETWORK() {
+	# Tries to ping google dns server, if successful return Online, if not return Offline
+	online_status=$(if ping -c 1 8.8.8.8 &>/dev/null; then echo -e "${GREEN}Online${ENDCOLOR}"; else echo -e "${RED}Offline${ENDCOLOR}"; fi)
+
+	# Show every IPv4 adress, filter only for the numbers, remove loopback adress, show remaining adress
+	local_ip=$(ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v '127.0.0.1' | head -n 1)
+
+	echo -e "\t│   ${RED}Internet${ENDCOLOR}  │ $online_status"
+	echo -e "\t│   ${MAGENTA}Local IP${ENDCOLOR}  │ $local_ip"
+	echo -e "\t├─────────────┤"
+}
+
+BOTTOM_BAR() {
+	echo -e "\t│   colors    │  ${WHITE} ${RED} ${YELLOW} ${GREEN} ${CYAN} ${BLUE} ${MAGENTA} ${BLACK}${ENDCOLOR}"
+	echo -e "\t╰─────────────╯"
+	echo -e ""
 }
 
 # Function to find temperature file paths
 FIND_TEMP_PATH() {
 	local potential_paths=("$@")
-		for path in "${potential_paths[@]}"; do
-			if [ -f "$path" ] && [ -s "$path" ]; then
-				echo "$path"
-				return 0
-			fi
-		done
+	for path in "${potential_paths[@]}"; do
+		if [ -f "$path" ] && [ -s "$path" ]; then
+			echo "$path"
+			return 0
+		fi
+	done
 	echo ""
 }
 
 TEMPS() {
-	echo -e "│	┌─[\e[1;35mTemperatures\e[0m]"
-
+	echo -e ""
+	echo -e "\t> Temperatures"
 	potential_paths_zone0=(
 		"/sys/class/thermal/thermal_zone0/temp"
 		"/sys/class/hwmon/hwmon0/temp1_input"
@@ -91,7 +102,7 @@ TEMPS() {
 		if [[ $temp0 =~ ^[0-9]+$ ]]; then
 			thermal_zone0=$(cat /sys/class/thermal/thermal_zone0/type 2>/dev/null || echo "Zone 0")
 			thermal_zone0_temp=$((temp0 / 1000))
-			echo -e "│	├ <\e[1;34m$thermal_zone0\e[0m> \e[1;36m${thermal_zone0_temp}°C\e[0m"
+			echo -e "\t${RED}$thermal_zone0${ENDCOLOR}: ${CYAN}${thermal_zone0_temp}°C${ENDCOLOR}"
 		fi
 	fi
 
@@ -100,25 +111,40 @@ TEMPS() {
 		if [[ $temp1 =~ ^[0-9]+$ ]]; then
 			thermal_zone1=$(cat /sys/class/thermal/thermal_zone1/type 2>/dev/null || echo "Zone 1")
 			thermal_zone1_temp=$((temp1 / 1000))
-			echo -e "│	├ <\e[1;34m$thermal_zone1\e[0m> \e[1;36m${thermal_zone1_temp}°C\e[0m"
+			echo -e "\t${RED}$thermal_zone1${ENDCOLOR}: ${CYAN}${thermal_zone1_temp}°C${ENDCOLOR}"
 		fi
 	fi
 
 	# If no temperature sensors where found
 	if [ -z "$thermal_zone0_path" ] && [ -z "$thermal_zone1_path" ]; then
-		echo -e "│	├ <\e[1;31mNo temperature data available\e[0m>"
+		echo -e "${RED}Error:${ENDCOLOR} No valid temperature sensor found"
 	fi
 
-	echo -e "│"
+	echo -e ""
 }
 
-BOTTOM_BAR() {
-	echo -e "└───┤< \e[1;31mVersion $VERSION\e[0m >"
-}
-
-TOP_BAR
-USAGE
-NETWORK
-OS
-TEMPS
-BOTTOM_BAR
+case $1 in
+"-h" | "--help")
+	echo -e "usage:  system-monitor [...]"
+	echo -e "arguments:"
+	echo -e "    -h | --help"
+	echo -e "    -t | --temps"
+	echo -e ""
+	exit 0
+	;;
+"-t" | "--temps")
+	TEMPS
+	exit 0
+	;;
+"")
+	TOP_BAR
+	OS
+	USAGE
+	NETWORK
+	BOTTOM_BAR
+	;;
+*)
+	echo -e "${RED}Error:${ENDCOLOR} unrecognized option '$1'. use -h"
+	exit 1
+	;;
+esac
